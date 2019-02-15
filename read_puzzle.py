a3a0829bc71b0f669dc09cd3d9beee1c850f90f8
Daniel Laszlo <daniel.laszlo94@gmail.com>
2019-02-15 20:43:52 +0100

    Use variable class

--- read_puzzle.py
+++ read_puzzle.py
@@ -1,7 +1,21 @@
 from pprint import pprint
+from collections import namedtuple
 
 puzzle_file = "1000 sudokus"
 rules_file = "sudoku-rules"
+
+
+class Variable:
+    def __init__(self, variable):
+        if variable[0] == '-':
+            self.ispositive = False
+            self.name = variable[1:]
+        else:
+            self.ispositive = True
+            self.name = variable
+
+    def __repr__(self):
+        return '-' + self.name if self.ispositive else self.name
 
 
 def read_puzzles(puzzle_file):
@@ -21,7 +35,7 @@ def encode_puzzle(puzzle):
 def read_dimacs(dimacs_file):
     with open(dimacs_file + ".txt", "r") as f:
         for line in f:
-            if line[0] != "p" or line[0] != "c":
+            if line[0] != "p" and line[0] != "c":
                 yield line
 
 
@@ -29,12 +43,14 @@ def parse_dimacs(dimacs_generator):
     result = []
     for line in dimacs_generator:
         clause = line.split()[:-1]
+        clause = [Variable(var) for var in clause]
         result.append(clause)
     return result
 
 
 def solve(puzzle_file, rules_file):
     rules = parse_dimacs(read_dimacs(rules_file))
+    print(rules)
     for puzzle in read_puzzles(puzzle_file):
         parsed_puzzle = parse_dimacs(encode_puzzle(puzzle))
         # solve_puzzle(rules + parsed_puzzle)
